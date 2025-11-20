227727b2652b5fbc7def95487c8219244b136fc9
tohru97 <tohru970211@gmail.com>
2025-11-20 22:51:46 +0900

    유니티넷코드를한번써보자

--- cc_1/Assets/Script/Network/NetworkController.cs
+++ cc_1/Assets/Script/Network/NetworkController.cs
@@ -14,25 +14,65 @@ public class NetworkController : SingletonMono<NetworkController>, IInitializabl
     [SerializeField]
     private UnityTransport _unityTransport;
 
+    #region Host Variables
     private bool _isHost = false;
+
+    private NetPlayer _hostPlayer;
+    private NetPlayer _remotePlayer;
+    #endregion
 
     public UniTask InitializeAsync()
     {
         Debug.Log("NetworkController Initialized");
+
+        _unityTransport.ConnectionData.Address = "127.0.0.1";
+        _unityTransport.ConnectionData.Port = 7777;
+
+        StartHost();
+
         return UniTask.CompletedTask;
+    }
+
+    private void SetNetworkManagerCallback()
+    {
+        _networkManager.OnServerStarted += () =>
+        {
+            Debug.Log("Server started.");
+        };
+
+        _networkManager.OnServerStopped += (isStop) =>
+        {
+            Debug.Log("Server stopped.");
+        };
+
+        _networkManager.OnClientConnectedCallback += (clientId) =>
+        {
+            Debug.Log($"Client connected: {clientId}");
+        };
+
+        _networkManager.OnClientDisconnectCallback += (clientId) =>
+        {
+            Debug.Log($"Client disconnected: {clientId}");
+        };
     }
 
     #region Host
     public void StartHost()
     {
         _isHost = true;
+        _networkManager.StartServer();
+        _networkManager.StartHost();
     }
     #endregion
 
     #region Client
     public void JoinMatch()
     {
+        _unityTransport.ConnectionData.Address = "127.0.0.1";
+        _unityTransport.ConnectionData.Port = 7777;
 
+        _isHost = false;
+        _networkManager.StartClient();
     }
     #endregion
 
--- cc_1/Assets/Script/Scene/LobbyScene.cs
+++ cc_1/Assets/Script/Scene/LobbyScene.cs
@@ -1,8 +1,12 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyScene : BaseScene
 {
+    [SerializeField]
+    private Button _testButton;
+
     public override eScene GetCurrentSceneType()
     {
         return eScene.LobbyScene;
@@ -11,5 +15,13 @@ public class LobbyScene : BaseScene
     public override UniTask LoadAsync()
     {
         return UniTask.CompletedTask;
+    }
+
+    public void Start()
+    {
+        _testButton.onClick.AddListener(() =>
+        {
+            NetworkController.Instance.JoinMatch();
+        });
     }
 }
